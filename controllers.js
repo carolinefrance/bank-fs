563444341783e47daf15335bb16c18e29b16ad21
cjfrance <linijini@gmail.com>
2023-06-14 23:09:26 -0400

    fixed spacing on controllers.js

--- controllers.js
+++ controllers.js
@@ -6,7 +6,11 @@ async function createUser (req, res) {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    const newUser = await User.create({name: name, email: email, password: password});
+    const newUser = await User.create({
+        name: name, 
+        email: email, 
+        password: password
+    });
     console.log(newUser);
     res.json(newUser);
 };
