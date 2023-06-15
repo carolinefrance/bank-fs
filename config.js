6ded81ea7be76506104e3285eb68b3c4a389a303
cjfrance <linijini@gmail.com>
2023-06-14 23:07:40 -0400

    added comments to config

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
-// REFACTORED: Added configuration file that connects to MongoDB "bank-fs" using Mongoose
-// Import Mongoose library to work with MongoDB
+// REFACTORED: Added config file that connects server to MongoDB "bank-fs" using Mongoose
+// Import Mongoose library (a Node.js package) to work with MongoDB
 const mongoose = require('mongoose');  
 
 async function connectDB() {
-    // Connect to MongoDB w/ MONGODB_URI env var in .env file
+    // full MONGODB_URI is in .env file
     await mongoose.connect(process.env.MONGODB_URI);
-    // When connected, print success message to console
     console.log("Connected to DB");  
 }
-// Export connectDB function to be accessed by other modules
+// Export connectDB function; can be accessed by other modules
 module.exports = connectDB;
