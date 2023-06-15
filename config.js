5f7651dd61caad5d58582e5402e6410442a34bb0
cjfrance <linijini@gmail.com>
2023-06-14 23:01:35 -0400

    added postman POST test, config comments

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose');
+// REFACTORED: Added configuration file that connects to MongoDB "bank-fs" using Mongoose
+// Import Mongoose library to work with MongoDB
+const mongoose = require('mongoose');  
+
 async function connectDB() {
-    await mongoose.connect(process.env.MONGODB_URI)
-    console.log("Connected to DB");
+    // Connect to MongoDB w/ MONGODB_URI env var in .env file
+    await mongoose.connect(process.env.MONGODB_URI);
+    // When connected, print success message to console
+    console.log("Connected to DB");  
 }
+// Export connectDB function to be accessed by other modules
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+/* Connect to MongoDB in VS Code: 
+View / Command Palette / MongoDB: Connect with Connection String / 
+mongodb+srv://Caroline:<password>@cluster0.jwmfoqf.mongodb.net/bank-fs
+*/
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -12,23 +16,38 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-/* below is starter code */
-app.get('/account/create/:name/:email/:password', function (req, res) {
+/* Postman collection "bank-fs" */
+/* Postman test successful: POST http://localhost:8080/api/create */
+app.post('/create', function (req, res) {
+  console.log('Create user request received.');
   res.send({
-    name: req.params.name,
-    email: req.params.email,
-    password: req.params.password
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
   });
 });
 
-app.get('/account/login/:email/:password', function (req, res) {
+/* Postman test failed: GET http://localhost:8080/api/create */
+/* below is starter code, changed "params" to "body" */
+app.get('/create/:name/:email/:password', function (req, res) {
+  console.log('Get user request received.');
   res.send({
-    email: req.params.email,
-    password: req.params.password
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
   });
 });
 
-app.get('/account/all', function (req, res) {
+app.get('/login/:email/:password', function (req, res) {
+  console.log('Get login request received.');
+  res.send({
+    email: req.body.email,
+    password: req.body.password
+  });
+});
+
+app.get('/all', function (req, res) {
+  console.log('Get all request received.');
   res.send({
     name: 'peter',
     email: 'peter@mit.edu',
@@ -42,19 +61,19 @@ app.listen(port, () => {
 });
 
 // Added middleware for error handling
-// Handle 404 errors
+// Handle unfound page error 404 
 app.use(function (req, res, next) {
     res.status(404).send('404: Page not found');
   });
   
-  // Handle other errors
+  // Handle internal server error 500
   app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('500: Internal Server Error');
-  });  
+  });
 
-/* To run server locally, node index.js
-    To test, http://localhost:8080/account/create/peter/peter@mit/secret
+/* To run server locally, node server.js
+    To test, http://localhost:8080/api/create/peter/peter@mit/secret
     Browser output is the JSON object
     {"name":"peter","email":"peter@mit","password":"secret"}
 */
